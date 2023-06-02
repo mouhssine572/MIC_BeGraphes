5a0566ddc5bab5f7b354524f1a59e208912b9dcb
oussamahannouch1 <120005789+oussamahannouch1@users.noreply.github.com>
2023-06-02 22:01:59 +0300

    Update AStarAlgorithm.java

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
@@ -1,9 +1,17 @@
 package org.insa.graphs.algorithm.shortestpath;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+import org.insa.graphs.algorithm.AbstractSolution.Status;
+import org.insa.graphs.algorithm.utils.*;
+import org.insa.graphs.model.*;
 
 public class AStarAlgorithm extends DijkstraAlgorithm {
-
     public AStarAlgorithm(ShortestPathData data) {
         super(data);
     }
-
+	protected LabelStar newLabel(Node node, ShortestPathData data) {
+		return new LabelStar(node,data);
+	}
 }
